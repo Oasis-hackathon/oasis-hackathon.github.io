707b571b650f7d28c0383571d235dbdbfa0ca176
GlassK <glassk0724@gmail.com>
2020-11-30 19:19:46 +0900

    add: mission, tf, registeration, faq

--- js/scripts.js
+++ js/scripts.js
@@ -60,4 +60,22 @@
     }
   });
 
+  var modal = document.getElementById('myModal');
+  var btn = document.getElementById("myBtn");
+  var span = document.getElementsByClassName("modal-close")[0];                                          
+
+  btn.onclick = function() {
+      modal.style.display = "block";
+  }
+
+  span.onclick = function() {
+      modal.style.display = "none";
+  }
+
+  window.onclick = function(event) {
+      if (event.target == modal) {
+          modal.style.display = "none";
+      }
+  }
+
 })(jQuery); // End of use strict
